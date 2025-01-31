049a26239b49797b2d215fab955bfdb9a460fd01
Cleboost <clement.balarot@gmail.com>
2025-01-31 20:59:32 +0100

    refactor: update entry point configuration and improve onSuccess script

--- tsup-dev.config.ts
+++ tsup-dev.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   format: ["cjs"],
-  entryPoints: ["src/index.ts"],
+  entry: ["src/index.ts"],
   outDir: "dist",
-  onSuccess: "cd playground && pnpm i @djs-core/cli && pnpm run test",
+  onSuccess: "cd playground && pnpm i && pnpm run test",
   dts: true,
   skipNodeModulesBundle: true,
   watch: true,
--- tsup-prod.config.ts
+++ tsup-prod.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   format: ["cjs"],
-  entryPoints: ["src/index.ts"],
+  entry: ["src/index.ts"],
   dts: true,
   shims: true,
   skipNodeModulesBundle: false,
